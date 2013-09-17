efefad8adf007eab8af01b4d4a7443d9dadcecd5
peternguyen93 <peternguyen199321@gmail.com>
2013-09-17 10:12:54 +0700

    New Version

--- extract_shellcode.sh
+++ extract_shellcode.sh
@@ -2,7 +2,7 @@
 #Author: peternguyen
 #extract shellcode from binary file
 filename=$1
-for i in `xxd -i $filename.bin | grep -v '\;' | grep -v unsigned | sed s/" "/" "/ | sed s/","/""/g | sed s/"0x"/"\\\\x"/g`
+for i in `xxd -i $filename | grep -v '\;' | grep -v unsigned | sed s/" "/" "/ | sed s/","/""/g | sed s/"0x"/"\\\\x"/g`
 do
     echo "Write to "$filename.shellcode
     echo -n "\\$i" >> $filename.shellcode
